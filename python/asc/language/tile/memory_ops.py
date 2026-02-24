04bd71cac746e0be307a39e7b71e6d464541ff03
kirill600 <rumyantsev.kirill@h-partners.com>
2026-02-24 20:05:19 +0800

    Add tile_id parameter to load and store operations
    Co-authored-by: kirill600<rumyantsev.kirill@h-partners.com>

--- python/asc/language/tile/memory_ops.py
+++ python/asc/language/tile/memory_ops.py
@@ -6,7 +6,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 
-from typing import Iterable
+from typing import Iterable, Optional, Tuple, List
 
 from ..._C import ir
 from ..core.dtype import KnownTypes as KT
@@ -16,16 +16,34 @@ from .tensor import Tensor
 from .tile import Tile
 
 
-def load(tensor: Tensor, offsets: Iterable[RuntimeInt], shape: Iterable[int], pad_value: RuntimeNumeric = 0) -> Tile:
+def infer_offsets(tensor_shape: Tuple[RuntimeInt], shape: Iterable[int], tile_id: Optional[Iterable[RuntimeInt]],
+                  offsets: Optional[Iterable[RuntimeInt]]) -> List[RuntimeInt]:
+    if tile_id is not None and offsets is not None:
+        raise ValueError("'tile_id' and 'offsets' cannot be used together")
+    if tile_id is None and offsets is None:
+        raise ValueError("either 'tile_id' or 'offsets' must be provided")
+    shape = tuple(shape)
+    if len(tensor_shape) != len(shape):
+        raise RuntimeError(f"rank of 'tensor_shape' must match rank of 'shape'")
+    if tile_id is not None:
+        return [_mat(idx * size, KT.int32).to_ir() for idx, size in zip(tile_id, shape)]
+    return [_mat(v, KT.int32).to_ir() for v in offsets]
+
+
+def load(tensor: Tensor, shape: Iterable[int], tile_id: Optional[Iterable[RuntimeInt]] = None,
+         offsets: Optional[Iterable[RuntimeInt]] = None, pad_value: RuntimeNumeric = 0) -> Tile:
     if not all(isinstance(dim, int) for dim in shape):
         raise RuntimeError("shape must be integers")
+    offsets = infer_offsets(tensor.shape, shape, tile_id, offsets)
     ir_type = ir.get_asctile_TileType(list(shape), tensor.dtype.to_ir(), ir.TileLocation.UB)
-    offsets = [_mat(v, KT.int32).to_ir() for v in offsets]
     pad_value = _mat(pad_value, tensor.dtype).to_ir() if pad_value is not None else None
     handle = global_builder.get_ir_builder().create_asctile_LoadOp(ir_type, tensor.to_ir(), offsets, pad_value)
     return Tile(handle)
 
 
-def store(tile: Tile, tensor: Tensor, offsets: Iterable[RuntimeInt]) -> None:
-    offsets = [_mat(v, KT.int32).to_ir() for v in offsets]
+def store(tile: Tile, tensor: Tensor, tile_id: Optional[Iterable[RuntimeInt]] = None,
+          offsets: Optional[Iterable[RuntimeInt]] = None) -> None:
+    if not all(isinstance(dim, int) for dim in tile.shape):
+        raise RuntimeError("shape must be integers")
+    offsets = infer_offsets(tensor.shape, tile.shape, tile_id, offsets)
     global_builder.get_ir_builder().create_asctile_StoreOp(tile.to_ir(), tensor.to_ir(), offsets)
