9889c8f376eda6ef2250a527728466f5b40e0419
vla5924 <vlasov.maxim@huawei-partners.com>
2026-03-05 22:33:28 +0800

    Add supported kernels for Ascend910_95: vadd, vmuladd, softmax, simple matmul
    Co-authored-by: vla5924<vlasov.maxim@huawei-partners.com>

---
+++ python/test/kernels/asc2/test_vmuladd.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+
+import numpy as np
+
+import asc
+import asc.runtime.config as config
+import asc2
+
+
+@asc2.jit(always_compile=True)
+def vmuladd_kernel(x_ptr: asc.GlobalAddress, y_ptr: asc.GlobalAddress, z_ptr: asc.GlobalAddress,
+                   out_ptr: asc.GlobalAddress, size: int, tile_size: asc.ConstExpr[int],
+                   tile_per_block: asc.ConstExpr[int], buffer_factor: asc.ConstExpr[int]):
+    x_gm = asc2.tensor(x_ptr, [size])
+    y_gm = asc2.tensor(y_ptr, [size])
+    z_gm = asc2.tensor(z_ptr, [size])
+    out_gm = asc2.tensor(out_ptr, [size])
+    base_offset = asc.get_block_idx() * tile_size * tile_per_block
+    for i in asc2.range(tile_per_block, unroll_factor=buffer_factor):
+        tile_offset = base_offset + i * tile_size
+        x = asc2.load(x_gm, [tile_size], offsets=[tile_offset])
+        y = asc2.load(y_gm, [tile_size], offsets=[tile_offset])
+        z = asc2.load(z_gm, [tile_size], offsets=[tile_offset])
+        out = x * y + z
+        asc2.store(out, out_gm, offsets=[tile_offset])
+
+
+def vmuladd_launch(x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
+    out = np.empty_like(x)
+    size = out.size
+    core_num = 16
+    tile_size = 32
+    num_tiles = asc.ceildiv(size, tile_size)
+    vmuladd_kernel[core_num](x, y, z, out, size, tile_size, asc.ceildiv(num_tiles, core_num), buffer_factor=2)
+    return out
+
+
+def test_vmuladd(backend: config.Backend, platform: config.Platform):
+    config.set_platform(backend, platform)
+    rng = np.random.default_rng(seed=2026)
+    size = 8192
+    x = rng.random(size, dtype=np.float32) * 10
+    y = rng.random(size, dtype=np.float32) * 10
+    z = rng.random(size, dtype=np.float32) * 10
+    out = vmuladd_launch(x, y, z)
+    np.testing.assert_allclose(out, x * y + z)
