87a214dbeb45dea71a0469446f59b5c3b344b4c4
artyomka <kuznetsov.artyom@h-partners.com>
2026-03-05 21:21:38 +0800

    Add lowering L3, L2 to L0 patterns and LowerToL0Pass
    Co-authored-by: artyomka<kuznetsov.artyom@h-partners.com>

---
+++ lib/Dialect/Asc/Transforms/LowerToL0.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under
+ * the terms and conditions of CANN Open Software License Agreement Version 2.0
+ * (the "License"). Please refer to the License for details. You may not use
+ * this file except in compliance with the License. THIS SOFTWARE IS PROVIDED ON
+ * AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS
+ * FOR A PARTICULAR PURPOSE. See LICENSE in the root of the software repository
+ * for the full text of the License.
+ */
+
+#include "ascir/Dialect/Asc/Transforms/Passes.h"
+#include "ascir/Dialect/Asc/IR/Asc.h"
+#include "ascir/Dialect/EmitAsc/IR/EmitAsc.h"
+#include "ascir/Dialect/Utils/ConstantOpBuilder.h"
+
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+namespace mlir {
+namespace ascendc {
+#define GEN_PASS_DEF_LOWERTOL0
+#include "ascir/Dialect/Asc/Transforms/Passes.h.inc"
+} // namespace ascendc
+} // namespace mlir
+
+using namespace mlir;
+using namespace ascendc;
+
+namespace {
+template <typename L2Op, typename L0Op>
+LogicalResult lowerUnaryOp(L2Op op, PatternRewriter& rewriter)
+{
+    ascir::ConstantOpBuilder consts(rewriter);
+    auto loc = op.getLoc();
+    auto zero = consts.i64(0);
+    auto mask = rewriter.create<emitasc::MaskOp>(loc, zero, zero);
+    auto repeatParams = rewriter.create<ConstructOp>(loc, rewriter.getType<UnaryRepeatParamsType>());
+    rewriter.replaceOpWithNewOp<L0Op>(op, op.getDst(), op.getSrc(), mask, zero, repeatParams);
+    return success();
+}
+
+template <typename L2L3Op, typename L0Op>
+LogicalResult lowerBinaryOp(L2L3Op op, PatternRewriter& rewriter)
+{
+    ascir::ConstantOpBuilder consts(rewriter);
+    auto loc = op.getLoc();
+    auto zero = consts.i64(0);
+    auto mask = rewriter.create<emitasc::MaskOp>(loc, zero, zero);
+    auto repeatParams = rewriter.create<ConstructOp>(loc, rewriter.getType<BinaryRepeatParamsType>());
+    rewriter.replaceOpWithNewOp<L0Op>(op, op.getDst(), op.getSrc0(), op.getSrc1(), mask, zero, repeatParams);
+    return success();
+}
+
+struct LowerToL0Pass : public ascendc::impl::LowerToL0Base<LowerToL0Pass> {
+    void runOnOperation() override
+    {
+        auto funcOp = getOperation();
+        RewritePatternSet patterns(funcOp.getContext());
+        populateLowerToL0Patterns(patterns);
+        if (applyPatternsAndFoldGreedily(funcOp, std::move(patterns)).failed())
+            signalPassFailure();
+    }
+};
+} // namespace
+
+namespace mlir {
+namespace ascendc {
+void populateLowerToL0Patterns(RewritePatternSet& patterns)
+{
+    patterns.add(lowerUnaryOp<AbsL2Op, AbsL0Op>);
+    patterns.add(lowerUnaryOp<ExpL2Op, ExpL0Op>);
+    patterns.add(lowerUnaryOp<LnL2Op, LnL0Op>);
+    patterns.add(lowerUnaryOp<NotL2Op, NotL0Op>);
+    patterns.add(lowerUnaryOp<ReciprocalL2Op, ReciprocalL0Op>);
+    patterns.add(lowerUnaryOp<ReluL2Op, ReluL0Op>);
+    patterns.add(lowerUnaryOp<RsqrtL2Op, RsqrtL0Op>);
+    patterns.add(lowerUnaryOp<SqrtL2Op, SqrtL0Op>);
+    patterns.add(lowerBinaryOp<AddL2Op, AddL0Op>);
+    patterns.add(lowerBinaryOp<AddL3Op, AddL0Op>);
+    patterns.add(lowerBinaryOp<AddDeqReluL2Op, AddDeqReluL0Op>);
+    patterns.add(lowerBinaryOp<AddReluL2Op, AddReluL0Op>);
+    patterns.add(lowerBinaryOp<AndL2Op, AndL0Op>);
+    patterns.add(lowerBinaryOp<DivL2Op, DivL0Op>);
+    patterns.add(lowerBinaryOp<DivL3Op, DivL0Op>);
+    patterns.add(lowerBinaryOp<FusedMulAddL2Op, FusedMulAddL0Op>);
+    patterns.add(lowerBinaryOp<FusedMulAddReluL2Op, FusedMulAddReluL0Op>);
+    patterns.add(lowerBinaryOp<MaxL2Op, MaxL0Op>);
+    patterns.add(lowerBinaryOp<MinL2Op, MinL0Op>);
+    patterns.add(lowerBinaryOp<MulL2Op, MulL0Op>);
+    patterns.add(lowerBinaryOp<MulL3Op, MulL0Op>);
+    patterns.add(lowerBinaryOp<MulAddDstL2Op, MulAddDstL0Op>);
+    patterns.add(lowerBinaryOp<OrL2Op, OrL0Op>);
+    patterns.add(lowerBinaryOp<SubL2Op, SubL0Op>);
+    patterns.add(lowerBinaryOp<SubL3Op, SubL0Op>);
+    patterns.add(lowerBinaryOp<SubReluL2Op, SubReluL0Op>);
+}
+
+std::unique_ptr<Pass> createLowerToL0Pass() { return std::make_unique<LowerToL0Pass>(); }
+} // namespace ascendc
+} // namespace mlir
