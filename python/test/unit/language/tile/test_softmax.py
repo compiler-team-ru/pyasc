0351b5d7763289f2be9c049e06287254ca6d8dcd
vla5924 <vlasov.maxim@huawei-partners.com>
2026-03-20 16:54:22 +0800

    Support Ascend910_9589 platform
    Co-authored-by: vla5924<vlasov.maxim@huawei-partners.com>

--- python/test/unit/language/tile/test_softmax.py
+++ python/test/unit/language/tile/test_softmax.py
@@ -7,9 +7,8 @@ import asc2
 
 
 @pytest.fixture(autouse=True)
-def set_platform(backend: config.Backend, platform: config.Platform):
-    if platform != config.Platform.Ascend910_9599:
-        pytest.skip("platform is not supported")
+def set_platform(backend: config.Backend, platform: config.Platform, require_platform_95):
+    require_platform_95(platform)
     config.set_platform(backend, platform, check=False)
 
 
