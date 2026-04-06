996b894633cd76f957a0b7ae7bd4ce9e77529c27
kirill600 <rumyantsev.kirill@h-partners.com>
2026-04-06 19:53:06 +0800

    Rename "value" to "count" to match CountMaskOp definition
    Co-authored-by: kirill600<rumyantsev.kirill@h-partners.com>

--- python/src/OpBuilder.cpp
+++ python/src/OpBuilder.cpp
@@ -1001,9 +1001,9 @@ void bindCreateAscTileOperations(py::class_<PyOpBuilder>& clss)
 
     clss.def(
             "create_asctile_CountMaskOp",
-            [](PyOpBuilder& self, Value& value, std::optional<Value> other) -> asctile::CountMaskOp {
+            [](PyOpBuilder& self, Value& count, std::optional<Value> other) -> asctile::CountMaskOp {
                 Value otherVal = other.has_value() ? *other : Value();
-                return self.create<asctile::CountMaskOp>(value, otherVal);
+                return self.create<asctile::CountMaskOp>(count, otherVal);
             },
             py::arg("count"), py::arg("other") = py::none())
         .def(
