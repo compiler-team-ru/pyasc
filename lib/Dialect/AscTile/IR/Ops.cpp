1ae8930ff5cfbb58d319328eccbfbdc8a84cca2e
IlyaShein <shein.ilya@huawei-partners.com>
2026-04-23 18:52:08 +0800

    Support asc2.copy from L0C to L1 for float16 and bfloat16

--- lib/Dialect/AscTile/IR/Ops.cpp
+++ lib/Dialect/AscTile/IR/Ops.cpp
@@ -202,6 +202,27 @@ LogicalResult StoreFixpipeOp::verify()
 }
 
 //===----------------------------------------------------------------------===//
+// CopyFixpipeOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult CopyFixpipeOp::canonicalize(CopyFixpipeOp op, PatternRewriter& rewriter)
+{
+    if (op->getUses().empty()) {
+        rewriter.eraseOp(op);
+        return success();
+    }
+    return failure();
+}
+
+LogicalResult CopyFixpipeOp::verify()
+{
+    if (!getQuantize() && getElementTypeOrSelf(getBase()) != getElementTypeOrSelf(getResult())) {
+        return emitOpError("failed to verify that all of {base, result} have same element type");
+    }
+    return success();
+}
+
+//===----------------------------------------------------------------------===//
 // AscTileDialect
 //===----------------------------------------------------------------------===//
 
