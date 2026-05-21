b1a020acb25c2d1e41f17e58718c31598fa2bdb5
IlyaShein <shein.ilya@huawei-partners.com>
2026-05-21 21:37:54 +0800

    Handle asctile.load with multiple users in CubeTransposeToLoad pass
    Fix CubeTransposeToLoad pass for unroll_factor=2

--- lib/Dialect/AscTile/Transforms/CubeTransposeToLoad.cpp
+++ lib/Dialect/AscTile/Transforms/CubeTransposeToLoad.cpp
@@ -28,35 +28,56 @@ using namespace mlir::asctile;
 
 namespace {
 
+constexpr const char* const fusibleAttr = "asctile.fusible";
+
+void markTranspose(asctile::TransposeOp op)
+{
+    auto tileLoc = op.getType().getLoc();
+    if (tileLoc != TileLocation::L0A && tileLoc != TileLocation::L0B)
+        return;
+    auto copyOp = op.getOperand().getDefiningOp<asctile::CopyOp>();
+    if (!copyOp || !copyOp->hasOneUse())
+        return;
+    if (copyOp.getType().getShape().size() != 2)
+        return;
+    auto* loadOp = copyOp.getBase().getDefiningOp();
+    if (!loadOp)
+        return;
+    if (!loadOp->hasOneUse()) {
+        for (auto* useLoad : loadOp->getUsers()) {
+            auto copyFromLoad = dyn_cast<asctile::CopyOp>(useLoad);
+            if (!copyFromLoad || !copyFromLoad->hasOneUse())
+                return;
+            if (!llvm::all_of(
+                    copyFromLoad->getUsers(), [](auto* useCopy) { return isa<asctile::TransposeOp>(useCopy); }))
+                return;
+        }
+    }
+    op->setAttr(fusibleAttr, UnitAttr::get(op.getContext()));
+}
+
 struct CubeTransposeToLoad : OpRewritePattern<asctile::TransposeOp> {
     using OpRewritePattern::OpRewritePattern;
 
     LogicalResult matchAndRewrite(asctile::TransposeOp op, PatternRewriter& rewriter) const override
     {
+        if (!op->hasAttrOfType<UnitAttr>(fusibleAttr))
+            return failure();
         auto opType = op.getType();
-        auto tileLoc = opType.getLoc();
-        if (tileLoc != TileLocation::L0A && tileLoc != TileLocation::L0B)
-            return failure();
         auto copyOp = op.getOperand().getDefiningOp<asctile::CopyOp>();
-        if (!copyOp || !copyOp->hasOneUse())
-            return failure();
-        const auto* attr = tileLoc == TileLocation::L0A ? asctile::attr::transposeA : asctile::attr::transposeB;
+        const auto* attr = opType.getLoc() == TileLocation::L0A ? asctile::attr::transposeA : asctile::attr::transposeB;
+        auto* loadOp = copyOp.getBase().getDefiningOp();
+        rewriter.startOpModification(loadOp);
+        loadOp->setAttr(attr, rewriter.getUnitAttr());
+        rewriter.finalizeOpModification(loadOp);
         auto copyOpType = copyOp.getType();
         auto shape = copyOpType.getShape();
-        if (shape.size() != 2)
-            return failure();
         Type newType = TileType::get({shape[1], shape[0]}, opType.getElementType(), copyOpType.getLoc());
         auto newCopyOp = rewriter.create<asctile::CopyOp>(op.getLoc(), newType, copyOp.getBase(), copyOp.getOffsets());
         rewriter.replaceOp(copyOp, newCopyOp);
         rewriter.startOpModification(newCopyOp);
         newCopyOp->setAttr(attr, rewriter.getUnitAttr());
         rewriter.finalizeOpModification(newCopyOp);
-        auto* loadOp = newCopyOp.getBase().getDefiningOp();
-        if (!loadOp || !loadOp->hasOneUse())
-            return failure();
-        rewriter.startOpModification(loadOp);
-        loadOp->setAttr(attr, rewriter.getUnitAttr());
-        rewriter.finalizeOpModification(loadOp);
         rewriter.replaceOp(op, newCopyOp);
         return success();
     }
@@ -66,6 +87,7 @@ struct CubeTransposeToLoadPass : public asctile::impl::CubeTransposeToLoadBase<C
     void runOnOperation() override
     {
         func::FuncOp funcOp = getOperation();
+        funcOp->walk(markTranspose);
         MLIRContext* context = &getContext();
         RewritePatternSet patterns(context);
         patterns.insert<CubeTransposeToLoad>(context);
