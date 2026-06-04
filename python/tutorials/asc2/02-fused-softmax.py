0717d879e9328f8aa06bd286c78348e3a9f33620
alexflegontov <flegontov.alexander@huawei.com>
2026-06-04 15:46:23 +0800

    Add vadd, softmax, matmul tutorials for PyAsc2
    Add PyAsc2 tutorials:
    - vadd
    - softmax
    - matmul

--- python/tutorials/asc2/02-fused-softmax.py
+++ python/tutorials/asc2/02-fused-softmax.py
@@ -6,10 +6,10 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 """
-Vector Addition
-===============
+Fused Softmax
+=============
 
-This tutorial demonstrates vector addition operator and its launch on Ascend simulator.
+This tutorial demonstrates softmax operator and its launch on Ascend simulator.
 
 """
 
@@ -20,39 +20,40 @@ import torch
 # The functions which are executed on Ascend NPU must be marked with `@asc2.jit` decorator.
 # Available parameters for @asc2.jit decorator can be seen in the documentation:
 # https://compiler-team-ru.github.io/pyasc/python-api/rst/runtime/index.html
-@asc2.jit
-def vector_add(
+@asc2.jit(reuse_ub=True)
+def fused_softmax(
         # Pointers to input and output tensors should have `asc2.GlobalAddress` type.
-        x_ptr: asc2.GlobalAddress, y_ptr: asc2.GlobalAddress, out_ptr: asc2.GlobalAddress,
+        input_ptr: asc2.GlobalAddress, output_ptr: asc2.GlobalAddress,
         # Scalar parameters are passed as Python types (e.g. `int`, `float`).
         # For optimization purposes it is recommended to pass scalar parameter as constants (e.g. `asc2.ConstExpr[int]`).
-        size: int,
-        # tile_size is used in asc2.load must be asc2.ConstExpr
-        tile_size: asc2.ConstExpr):
+        # Here num_rows and num_cols are compiler-time parameter:
+        num_rows: asc2.ConstExpr, num_cols: asc2.ConstExpr,
+        # tile_shape is a list and must be asc2.ConstExpr
+        tile_shape: asc2.ConstExpr):
 
     # Tensor descriptor is created from `asc2.GlobalAddress` to represent entire tensor.
-    x_gm = asc2.tensor(x_ptr, [size])
-    y_gm = asc2.tensor(y_ptr, [size])
-    out_gm = asc2.tensor(out_ptr, [size])
+    in_gm = asc2.tensor(input_ptr, [num_rows, num_cols])
+    out_gm = asc2.tensor(output_ptr, [num_rows, num_cols])
 
-    # Python expressions are used to calculate offset and define the loop iterating over tiles:
+    # Python expressions are used to calculate offset:
     # `asc2.block_num()` function provides number of AICOREs launched.
-    block_loop_num = asc2.ceildiv(asc2.ceildiv(size, asc2.block_num()), tile_size)
+    rows_per_block = asc2.ceildiv(num_rows, asc2.block_num())
     # `asc2.block_idx()` function is used to get current AICORE index.
-    block_offset = asc2.block_idx() * tile_size * block_loop_num
+    block_offset = asc2.block_idx() * rows_per_block
+    # Define the loop iterating over tiles
+    ub_loop = asc2.number(asc2.ceildiv(rows_per_block, tile_shape[0]), asc2.int_)
 
     # `unroll_factor` parameter of `asc2.range` in `for` loop can be used to manage software pipelining. Set it to `2` to enable double buffering.
     # `parallel` parameter of `asc2.range` in `for` loop enable overlapping of store operation of `i`-th iteration and load of `i+1`-th iteration.
     # It is user responsibility to ensure that there are no data dependencies between overlapped iterations.
-    for i in asc2.range(block_loop_num, unroll_factor=2, parallel=True):
-        tile_offset = block_offset + i * tile_size
-        # `asc2.load` is used to create tile object which is used for further calculations. Data movement from GM to UB happens in this operation.
-        x = asc2.load(x_gm, [tile_size], offsets=[tile_offset])
-        y = asc2.load(y_gm, [tile_size], offsets=[tile_offset])
-        # One or more operations can be applied for tiles. It is compiler responsibility to allocate required number of memory blocks in UB.
-        out = x + y
+    for i in asc2.range(ub_loop, unroll_factor=2, parallel=True):
+        row_start_offset = block_offset + i * tile_shape[0]
+        # `asc2.load` is used to create 2D tile object to load from GM to UB and pad with '-inf' all values that are out of global tensor
+        rows = asc2.load(in_gm, [tile_shape[0], tile_shape[1]], offsets=[row_start_offset, 0], pad_value=float('-inf'))
+        # Call high-level 2D softmax
+        out = asc2.softmax(rows)
         # `asc2.store` is used to move data from UB back to GM.
-        asc2.store(out, out_gm, offsets=[tile_offset])
+        asc2.store(out, out_gm, offsets=[row_start_offset, 0])
 
 
 if __name__ == "__main__":
@@ -61,15 +62,22 @@ if __name__ == "__main__":
     device_id = 0  # might be necessary to provide if more than one NPU device is present in the system
     asc2.set_platform(backend, soc_version, device_id)
 
-    size = 8192
-    block_num = 16
-    tile_size = 128
+    input_shape_2d = [256, 98]
+    rows_per_iter = 5
+    block_num = 56
     dtype = torch.float32
+
+    # Alignment for tile_shape
+    num_rows, num_cols = input_shape_2d
+    ALIGNMENT_ELEMENTS = 32 // dtype.itemsize
+    tile_shape = [rows_per_iter, asc2.ceildiv(num_cols, ALIGNMENT_ELEMENTS) * ALIGNMENT_ELEMENTS]
+
     # Allocate tensors
-    x = torch.randn(size, dtype=dtype)
-    y = torch.randn(size, dtype=dtype)
-    out = torch.empty_like(x)
+    in_tensor = torch.randn(input_shape_2d, dtype=dtype)
+    out_tensor = torch.zeros(input_shape_2d, dtype=dtype)
 
     # For the kernel invocation, number of AICOREs should be provided in brackets:
-    vector_add[block_num](x, y, out, size, tile_size)
-    torch.testing.assert_close(out, x + y)
+    fused_softmax[block_num](in_tensor, out_tensor, input_shape_2d[0], input_shape_2d[1], tile_shape)
+
+    expected = torch.softmax(in_tensor, dim=1)
+    torch.testing.assert_close(out_tensor, expected)
---
+++ python/tutorials/asc2/03-matrix-multiplication.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+"""
+Matrix Multiplication
+=====================
+
+This tutorial demonstrates matrix multiplication operator and its launch on Ascend simulator.
+
+"""
+
+import asc2
+import torch
+
+
+# The functions which are executed on Ascend NPU must be marked with `@asc2.jit` decorator.
+# Available parameters for @asc2.jit decorator can be seen in the documentation:
+# https://compiler-team-ru.github.io/pyasc/python-api/rst/runtime/index.html
+@asc2.jit
+def matrix_multiplication(
+        # Pointers to input and output tensors should have `asc2.GlobalAddress` type.
+        a_ptr: asc2.GlobalAddress, b_ptr: asc2.GlobalAddress, c_ptr: asc2.GlobalAddress,
+        # Input shapes for A and B matrices
+        a_shape: asc2.ConstExpr, b_shape: asc2.ConstExpr,
+        # For optimization purposes it is recommended to pass scalar parameter as constants (e.g. `asc2.ConstExpr[int]`).
+        single_core_m: asc2.ConstExpr, single_core_n: asc2.ConstExpr, step_ka: asc2.ConstExpr, step_kb: asc2.ConstExpr,
+        base_k: asc2.ConstExpr):
+    m, k = a_shape
+    _, n = b_shape
+    # Tensor descriptor is created from `asc2.GlobalAddress` to represent entire tensor.
+    a_gm = asc2.tensor(a_ptr, a_shape)
+    b_gm = asc2.tensor(b_ptr, b_shape)
+    c_gm = asc2.tensor(c_ptr, [m, n])
+
+    # Create 2D tile for cube L0C accumulator
+    acc = asc2.zeros_acc([single_core_m, single_core_n], dtype=asc2.float32)
+
+    # Python expressions are used to calculate block offset:
+    # - `asc2.block_idx()` function is used to get current AICORE index.
+    n_blocks = asc2.ceildiv(n, single_core_n)
+    m_off = single_core_m * (asc2.block_idx() / n_blocks)
+    n_off = single_core_n * (asc2.block_idx() % n_blocks)
+
+    # `unroll_factor` parameter of `asc2.range` in `for` loop can be used to manage software pipelining. Set it to `2` to enable double buffering.
+    # `parallel` parameter of `asc2.range` in `for` loop enable overlapping of store operation of `i`-th iteration and load of `i+1`-th iteration.
+    # It is user responsibility to ensure that there are no data dependencies between overlapped iterations.
+    for k_outer in range(asc2.ceildiv(k, step_kb), unroll_factor=2, parallel=True):
+        # Load B matrix from GM to L1 tile object
+        b_l1 = asc2.load(b_gm, [step_kb, single_core_n], offsets=[k_outer * step_kb, n_off],
+                         location=asc2.TileLocation.L1)
+        for k_mid in range(asc2.ceildiv(step_kb, step_ka), unroll_factor=2, parallel=True):
+            k_off = k_outer * step_kb + k_mid * step_ka
+            # Load A matrix from GM to L1 tile object
+            a_l1 = asc2.load(a_gm, [single_core_m, step_ka], offsets=[m_off, k_off], location=asc2.TileLocation.L1)
+            for k_l0 in range(asc2.ceildiv(step_ka, base_k), unroll_factor=2, parallel=True):
+                # Copy A matrix from L1 to L0A tile object
+                a_l0 = asc2.copy(a_l1, [single_core_m, base_k], offsets=[0, k_l0 * base_k],
+                                 location=asc2.TileLocation.L0A)
+                # Copy B matrix from L1 to L0B tile object
+                b_l0 = asc2.copy(b_l1, [base_k, single_core_n], offsets=[k_mid * step_ka + k_l0 * base_k, 0],
+                                 location=asc2.TileLocation.L0B)
+                # Perform matrix multiplication with updating accumulator
+                asc2.matmul_acc(acc, a_l0, b_l0)
+
+    # `asc2.store` is used to move data from L0C accumulator back to GM.
+    asc2.store(acc, c_gm, offsets=[m_off, n_off])
+
+
+if __name__ == "__main__":
+    backend = asc2.Backend.Model  # can be "Model" for simulator or "NPU" for device
+    soc_version = asc2.Platform.Ascend950PR_9599  # Device version
+    device_id = 0  # might be necessary to provide if more than one NPU device is present in the system
+    asc2.set_platform(backend, soc_version, device_id)
+
+    block_num = 16
+    dtype = torch.float32
+
+    m, k, n = 1024, 64, 16
+    single_core_m, single_core_n = 64, 16
+    step_ka, step_kb, base_k = 16, 64, 16
+
+    # Allocate tensors for A, B and result matrix C
+    a = torch.rand((m, k), dtype=dtype)
+    b = torch.rand((k, n), dtype=dtype)
+    c = torch.zeros((m, n), dtype=dtype)
+
+    matrix_multiplication[block_num](a, b, c, a.shape, b.shape, single_core_m, single_core_n, step_ka, step_kb, base_k)
+    c_ref = a @ b
+    torch.testing.assert_close(c, c_ref)
