42f65466118b005a8b6fd8b88cd9dc0bafc78e7b
sergey-gribov-sample-text <gribov.sergey@h-partners.com>
2026-06-29 19:26:58 +0800

    Support padding for transpose on load
    - Added padding for transpose on load
    - Correctly calculate amount of data when offset too large

--- python/test/asc2/operations/test_transpose.py
+++ python/test/asc2/operations/test_transpose.py
@@ -19,32 +19,40 @@ def require_c310_auto(require_c310):
 @pytest.mark.parametrize(
     "dtype",
     (torch.int8, torch.int16, torch.int32, torch.int64, torch.float16, torch.bfloat16, torch.float32, torch.float64))
-@pytest.mark.parametrize("permute, input_shape, real_shape", [
+@pytest.mark.parametrize("permute, input_shape, real_shape, offsets, pad_value", [
     # 2d cases
-    [[1, 0], [16, 32], [16, 32]],
+    [[1, 0], [16, 32], [16, 32], [0, 0], 0],
     # 3d cases
-    [[0, 2, 1], [2, 16, 32], [2, 16, 32]],
-    [[1, 2, 0], [8, 2, 32], [8, 2, 32]],
-    [[1, 0, 2], [2, 4, 32], [2, 4, 32]],
-    [[2, 0, 1], [2, 16, 32], [2, 16, 32]],
-    [[2, 1, 0], [8, 2, 32], [8, 2, 32]],
+    [[0, 2, 1], [2, 16, 32], [2, 16, 32], [0, 0, 0], 0],
+    [[1, 2, 0], [8, 2, 32], [8, 2, 32], [0, 0, 0], 0],
+    [[1, 0, 2], [2, 4, 32], [2, 4, 32], [0, 0, 0], 0],
+    [[2, 0, 1], [2, 16, 32], [2, 16, 32], [0, 0, 0], 0],
+    [[2, 1, 0], [8, 2, 32], [8, 2, 32], [0, 0, 0], 0],
     # 4d cases
-    [[0, 1, 3, 2], [2, 3, 16, 16], [2, 3, 16, 16]],
-    [[3, 2, 1, 0], [16, 2, 3, 16], [16, 2, 3, 16]],
-    [[3, 1, 2, 0], [32, 2, 3, 16], [32, 2, 3, 16]],
-    [[2, 0, 1, 3], [2, 3, 4, 32], [2, 3, 4, 32]],
-    [[0, 2, 1, 3], [2, 3, 4, 16], [2, 3, 4, 16]],
-    # default
-    [[], [64, 32], [64, 32]],
-    [[], [2, 32, 16], [2, 32, 16]],
-    [[], [3, 2, 32, 16], [3, 2, 32, 16]],
-    # with real_shape
-    [[2, 0, 1], [3, 32, 64], [3, 30, 50]],
-    [[0, 2, 1], [2, 64, 32], [1, 50, 10]],
-    [[3, 2, 0, 1], [16, 2, 16, 32], [10, 2, 10, 30]],
-    [[1, 0], [32, 64], [30, 60]],
+    [[0, 1, 3, 2], [2, 3, 16, 16], [2, 3, 16, 16], [0, 0, 0, 0], 0],
+    [[3, 2, 1, 0], [16, 2, 3, 16], [16, 2, 3, 16], [0, 0, 0, 0], 0],
+    [[3, 1, 2, 0], [32, 2, 3, 16], [32, 2, 3, 16], [0, 0, 0, 0], 0],
+    [[2, 0, 1, 3], [2, 3, 4, 32], [2, 3, 4, 32], [0, 0, 0, 0], 0],
+    [[0, 2, 1, 3], [2, 3, 4, 16], [2, 3, 4, 16], [0, 0, 0, 0], 0],
+    # test with empty permutation
+    [[], [64, 32], [64, 32], [0, 0], 0],
+    [[], [2, 32, 16], [2, 32, 16], [0, 0, 0], 0],
+    [[], [3, 2, 32, 16], [3, 2, 32, 16], [0, 0, 0, 0], 0],
+    # test when real_shape less than target buffer
+    [[2, 0, 1], [3, 32, 64], [3, 30, 50], [0, 0, 0], 0],
+    [[0, 2, 1], [2, 64, 32], [1, 50, 10], [0, 0, 0], 0],
+    [[3, 2, 0, 1], [16, 2, 16, 32], [10, 2, 10, 30], [0, 0, 0, 0], 0],
+    [[1, 0], [32, 64], [30, 60], [0, 0], 0],
+    # test when offset too large and tile is cutted
+    [[1, 0], [32, 32], [24, 24], [16, 0], 17],
+    [[0, 2, 1], [2, 32, 32], [2, 24, 24], [0, 0, 16], 17],
+    [[0, 1, 3, 2], [2, 2, 32, 32], [1, 1, 24, 24], [1, 0, 16, 0], 127],
+    # padding outer dimension only
+    [[0, 1, 3, 2], [4, 2, 32, 32], [4, 2, 32, 32], [3, 0, 0, 0], 17],
 ])
-def test_transpose(permute, input_shape, real_shape, dtype):
+def test_transpose_onload(permute, input_shape, real_shape, offsets, pad_value, dtype):
+    if dtype == torch.float64:
+        pytest.skip('Duplicate not supports double type')
     dim_order = permute
     if len(permute) == 0:
         dim_order = list(range(0, len(input_shape)))
@@ -52,6 +60,10 @@ def test_transpose(permute, input_shape, real_shape, dtype):
 
     output_shape = [input_shape[i] for i in dim_order]
     write_real_shape = [real_shape[i] for i in dim_order]
+
+    verify_copy_count = [min(max(0, input_shape[i] - offsets[i]), real_shape[i]) for i in range(0, len(dim_order))]
+    verify_pad_count = [input_shape[i] - verify_copy_count[i] for i in range(0, len(dim_order))]
+    print(f'Copy count {verify_copy_count} {verify_pad_count}')
     input = torch.rand(input_shape).mul(100).to(dtype=dtype)
     result = torch.zeros(output_shape, dtype=dtype)
     items_align = 32 // input.element_size()
@@ -60,18 +72,21 @@ def test_transpose(permute, input_shape, real_shape, dtype):
 
     golden = input
     for i in range(0, len(real_shape)):
-        golden = golden.narrow(i, 0, real_shape[i])
+        golden = golden.narrow(i, offsets[i], verify_copy_count[i])
+        if verify_pad_count[i] > 0:
+            shape = list(golden.shape)
+            shape[i] = verify_pad_count[i]
+            golden = torch.cat((golden, torch.full(shape, pad_value, dtype=golden.dtype)), dim=i)
     golden = golden.permute(dim_order)
 
     @asc2.jit(always_compile=True)
     def kernel(input_ptr, result_ptr, input_shape: asc2.ConstExpr, real_shape: asc2.ConstExpr,
-               output_shape: asc2.ConstExpr, write_real_shape: asc2.ConstExpr, permute: asc2.ConstExpr):
+               output_shape: asc2.ConstExpr, write_real_shape: asc2.ConstExpr, offsets: asc2.ConstExpr,
+               pad_value: asc2.ConstExpr, permute: asc2.ConstExpr):
         g_input = asc2.tensor(input_ptr, input_shape)
-        tile = asc2.load(g_input, [0] * len(input_shape), input_shape, real_shape=real_shape)
+        tile = asc2.load(g_input, offsets, input_shape, real_shape=real_shape, pad_value=pad_value)
         g_output = asc2.tensor(result_ptr, output_shape)
-        asc2.store(tile.transpose(*permute), g_output, [0] * len(output_shape), real_shape=write_real_shape)
+        asc2.store(tile.transpose(*permute), g_output, [0] * len(output_shape))
 
-    kernel[1](input, result, input_shape, real_shape, output_shape, write_real_shape, permute)
-    for i in range(0, len(write_real_shape)):
-        result = result.narrow(i, 0, write_real_shape[i])
+    kernel[1](input, result, input_shape, real_shape, output_shape, write_real_shape, offsets, pad_value, permute)
     torch.testing.assert_close(result, golden)
