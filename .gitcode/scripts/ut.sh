e870d7e80d791dd6327a82555055f69b1d52dd13
Larry_liuyi <larry.liuyi@huawei.com>
2026-07-22 14:56:55 +0800

    add actions file 增加代码化配置
    Co-authored-by: liuyi<larry.liuyi@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !142 merge master into master
    
    add actions file 增加代码化配置
    
    Created-by: Larry_liuyi
    Commit-by: liuyi
    Merged-by: cann-robot
    Description: ## 描述
    初始化增加代码化配置文件
    
    ## 关联的Issue
    https://gitcode.com/cann/pyasc/issues/37
    
    ## 测试
    工程能力，已验证通过
    
    ## 文档更新
    无
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x ] 其他，请描述：代码化配置
    
    
    
    
    See merge request: cann/pyasc!142

---
+++ .gitcode/scripts/ut.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+
+set +e
+
+echo "${ut_type:-}"
+echo "${TARGET_BRANCH:-}"
+echo "${obs_path:-}"
+
+grep -E "^VERSION_ID=" /etc/os-release | cut -d'"' -f2
+sudo update-alternatives --set gcc /usr/bin/gcc-14
+gcc --version
+
+# Print and execute command
+function LOG_DO() {
+   local date_time
+   date_time=$(date +%Y%m%d-%H%M%S)
+   echo -e "${BPurple}[Command]${Color_Off} ${date_time} ${Purple}$*${Color_Off}"
+   "$@"
+}
+
+main(){
+    local CURR_DIR REPO_HOME ret coverage_save
+    #获取参数
+    CURR_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
+    CURR_DIR=$(cd -P "${CURR_DIR}" && pwd -P) || exit 1
+    REPO_HOME=$(cd -P "${CURR_DIR}/../../../../" && pwd -P) || exit 1
+    #########
+    # install #
+    #########
+    source /home/jenkins/Ascend/cann/bin/setenv.bash
+    export LLVM_INSTALL_PREFIX=/home/jenkins/opensource/llvm
+    export LD_LIBRARY_PATH="${ASCEND_HOME_PATH}/tools/simulator/Ascend910B1/lib:${LD_LIBRARY_PATH:-}"
+
+    local obs_base="https://ascend-ci.obs.cn-north-4.myhuaweicloud.com/${obs_path}/cann-pyasc_linux-x86_64_ubuntu24.whl"
+    local p_name="pyasc-1.1.1+gf870bf7-cp310-cp310-linux_x86_64.whl"
+    if ! wget -O "${p_name}" "${obs_base}"; then
+        echo "Download ${p_name} from ${obs_base} failed"
+        exit 1
+    fi
+    if ! pip3 install "${p_name}"; then
+        echo "Install ${p_name} failed"
+        exit 1
+    fi
+
+    echo "Start run c++ testcase"
+    cd "${WORKSPACE}/test" || exit 1
+    coverage_save="false"
+    if [[ "${ut_type}" == "python" ]]; then
+        LOG_DO bash build_llt.sh --cov --run_python_ut --llvm_install_path "${LLVM_INSTALL_PREFIX}" -f "${WORKSPACE}/pr_filelist.txt"
+        ret=$?
+        if [[ ${ret} -eq 200 ]]; then
+            echo "not need run ut"
+            exit 0
+        fi
+    elif [[ "${ut_type}" == "cpp" ]]; then
+        export LIT_INSTALL_PATH="$(dirname "$(dirname "$(which lit)")")"
+        LOG_DO bash build_llt.sh --cov --check-ascir --llvm_install_path "${LLVM_INSTALL_PREFIX}" --lit_install_path "${LIT_INSTALL_PATH}" -f "${WORKSPACE}/pr_filelist.txt"
+        ret=$?
+        if [[ ${ret} -eq 200 ]]; then
+            echo "not need run ut"
+            exit 0
+        fi
+    else
+        echo "Unknown ut_type: '${ut_type:-}'"
+        exit 1
+    fi
+
+    if [[ ${ret} -ne 200 && ${ret} -ne 0 ]]; then
+        echo "run ut fail"
+        exit 1
+    fi
+    echo "ut_process=coverage" >> "${ATOMGIT_OUTPUT}"
+    exit 0
+}
+
+main "$@"
