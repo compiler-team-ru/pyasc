6354c1777ce23a666b0d6c60995b92b9507f72af
Larry_liuyi <larry.liuyi@huawei.com>
2026-08-22 14:41:18 +0800

    修改runs-on资源，优化actions配置
    Co-authored-by: liuyi<larry.liuyi@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !164 merge master into master
    
    修改runs-on资源，优化actions配置
    
    Created-by: Larry_liuyi
    Commit-by: liuyi
    Merged-by: cann-robot
    Description: ## 描述
    修改runs-on资源，优化actions配置
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：修改runs-on资源，优化actions配置
    
    
    
    
    See merge request: cann/pyasc!164

--- .gitcode/scripts/pre_smoke.sh
+++ .gitcode/scripts/pre_smoke.sh
@@ -11,7 +11,6 @@
 set -e
 set -o pipefail
 
-mkdir -p /home/taskspace && cd /home/taskspace
 echo "start run test case, please wait ..."
 
 export ASCEND_GLOBAL_LOG_LEVEL=2
@@ -32,11 +31,6 @@ conda deactivate
 mkdir -p /root/ascend
 slog_name="slog.tar.gz"
 tar -zcf slog.tar.gz -C /root/ascend log
-OBS_KEY="${obs_smoke_path}/plog/${slog_name}"
-# upload plog
-if python3 /home/upload.py --bucket-name "ascend-ci" --action upload  --local-file "slog.tar.gz" --obs-object-key "${OBS_KEY}"; then
-  echo "::set-output var=plog_url:https://ascend-ci.obs.cn-north-4.myhuaweicloud.com/${OBS_KEY}"
-fi
 
 date_time=$(date +%Y%m%d.%H%M%S)
 mkdir -p ./npu_log
